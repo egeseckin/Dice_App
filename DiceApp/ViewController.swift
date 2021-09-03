8aca898f2b5fde7d9ded49a854183a7381de45e2
egeseckin <ege.seckin@outlook.com>
2021-09-03 17:56:40 +0300

    Dice App Created

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
+import UIKit
+
 class ViewController: UIViewController {
 
+    //For referencing to the dice elements
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFive")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in:0...5)]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in:0...5)]
+ 
+    }
+    
 }
 
