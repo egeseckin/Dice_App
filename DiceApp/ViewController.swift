a62b6f09e3b955967ae856e3d22f19aeba6094cc
egeseckin <ege.seckin@outlook.com>
2021-09-03 17:26:55 +0300

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Noble on 3.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
